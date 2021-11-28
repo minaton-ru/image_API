e30a48a35cd3f46bf91460befb6469337763c41e
minaton-ru <minaton@yandex.ru>
2021-11-28 23:47:43 +0300

    Возможность загрузки нескольких файлов, авторизация

--- README.md
+++ README.md
@@ -2,3 +2,32 @@
 
 Используется Django Rest Framework  
  
+Загрузка нескольких изображений через API.  
+Загруженные файлы сохраняются в директорию `\media\images`  
+Файл сохраняется с уникальным именем.  
+В файле views.py строка 9 - декоратор для авторизации пользователя (строка заккоментирована).  
+
+## Описание API  
+Метод POST  
+URL - `http://127.0.0.1:8000/upload/`  
+BODY -  список file: [путь к загружаемому файлу]  
+
+## Запуск проекта  
+Скопировать файлы  
+```pip install -r requirements.txt```  
+```python manage.py makemigrations```  
+```python manage.py migrate```  
+```python manage.py runserver```  
+
+## Проверка работы метода API  
+### Postman  
+Метод POST  
+URL - `http://127.0.0.1:8000/upload/`  
+BODY:  
+Key - file  
+Value - [путь к загружаемому файлу]  
+
+### Модуль Requests
+```images = [('file', ('имяфайла.gif', open('/путь/к/файлу/имяфайла.gif', 'rb'))), ('file', ('имяфайла2.gif', open('/путь/к/файлу/имяфайла2.gif', 'rb')))]```  
+```response = requests.request("POST", "http://127.0.0.1:8000/upload/", files=images)```  
+```print(response.text)```  
--- img_upload/views.py
+++ img_upload/views.py
@@ -1,17 +1,23 @@
-from .models import Image
 from .serializers import ImageSerializer
 from rest_framework import generics
-from rest_framework.parsers import MultiPartParser
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import parser_classes
+from rest_framework.permissions import IsAuthenticated
 
+# @permission_classes([IsAuthenticated]) # включить этот декоратор - загрузка изображений будет доступна только авторизованным пользователям
+@parser_classes([MultiPartParser, FormParser])
 class ImageUploadView(generics.CreateAPIView):
-    parser_classes = [MultiPartParser]
 
-    def post(self, request, **args):
-        file_serializer = ImageSerializer(data=request.data)
-        if file_serializer.is_valid():
-            file_serializer.save()
-            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        files = request.FILES.getlist('file') # забираем список файлов из запроса
+        results = []
+        for file in files:
+            file_dict = {}
+            file_dict['file'] = file # переводим файл из запроса в словарь для сериализатора
+            file_serializer = ImageSerializer(data=file_dict)
+            if file_serializer.is_valid(raise_exception=True):
+                file_serializer.save()
+                results.append(file_serializer.data) # добавляем в список с результатами
+        return Response(results, status=status.HTTP_201_CREATED)
